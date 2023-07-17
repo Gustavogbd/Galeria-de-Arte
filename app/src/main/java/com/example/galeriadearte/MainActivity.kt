07ae2561f4b7fc76788fed8d2874165d05b1cb84
Gustavogbd <gustavokkg2@hotmail.com>
2023-07-17 13:47:26 -0300

    FrontEnd Layout

--- app/src/main/java/com/example/galeriadearte/MainActivity.kt
+++ app/src/main/java/com/example/galeriadearte/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.galeriadearte
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.galeriadearte.ui.theme.GaleriaDeArteTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,17 +47,65 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtGallery(){
     Column(modifier = Modifier.fillMaxSize()) {
+        ImageGallery()
+        ImageDescription()
+        ArtButtons()
+    }
+}
+@Composable
+fun ImageGallery(){
+    Row(
+        modifier = Modifier
+            .size(600.dp)
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.Center,
+
+    ) {
         Image(
             painter = painterResource(id = R.drawable.androidparty),
-            contentDescription = "Imagem"
+            contentDescription = "Art"
         )
-        Row() {
-            Text(text = "ArtWork Title")
-        }
     }
 }
 
+@Composable
+fun ImageDescription(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Text(
+            text = "Título",
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+        )
+        Text(text = "Nome (Ano)")
+    }
+}
 
+@Composable
+fun ArtButtons(){
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(width = 150.dp, height = 50.dp)
+        ) {
+            Text(text = "Anterior")
+        }
+        Spacer(modifier = Modifier.padding(8.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(width = 150.dp, height = 50.dp)
+        ) {
+            Text(text = "Próximo")
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
